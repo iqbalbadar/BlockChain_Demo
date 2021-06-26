a963bb543b9318b684434105dd9917fb7bea8335
Iqbal Badar <ibadar@ucsd.edu>
2021-06-25 23:13:45 -0700

    blockchain list class is updated, try and finish tomorrow

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,5 @@
 import hashlib
-from datetime import date
+import datetime
 
 class Block:
     def __init__(self, prev_hash, transactions , timestamp):
@@ -7,22 +7,23 @@ class Block:
         self.transactions = transactions
         self.timestamp = timestamp
         self._nonce = 0
-        hash = self.make_hash()
+        self.hash = self.make_hash()
 
     def __str__(self):
-        return str({"previous hash": self.prev_hash , "Transactions": str(self.transactions), "Timestamp" : self.timestamp})
+        return str({"previous hash": self.prev_hash , "Hash" : self.hash, "Transactions": str(self.transactions), "Timestamp" : self.timestamp})
     
     def make_hash(self):
         rawData = str(self.prev_hash) + str(self.timestamp) + str(self.transactions) + str(self._nonce)
-        return hashlib.sha256(rawData.encode())
+        return hashlib.sha256(rawData.encode()).hexdigest()
 
-    def mine_block(proof_of_work_diff):
+    def mine_block(proof_of_work_diff, miner, reward):
         hash_valid_temp = " " * proof_of_work_diff
         print("going through proof of work algo")
         while( hash[:proof_of_work_diff] != hash_valid_temp ):
             _nonce += 1
             hash = self.make_hash
         print("{0} this hash was mined" , hash )
+        return true
 
 class Transaction :
     def __init__(self, from_person, to_person, amount):
@@ -48,12 +49,12 @@ class BlockChain (object):
 
 
 def main():
-    print("hello")
     transaction_1 = Transaction("iqbal", "her", 11.56)
+    transaction_2 = Transaction("her", "iqbal", 12.56 )
 
-
-    b = Block("", transaction_1, date.today())
-    print(b)
+    b1 = Block("", transaction_1, datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+    b2 = Block(b1.hash, transaction_2, datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+    print(b2)
 
 if __name__ == "__main__":
     main()
