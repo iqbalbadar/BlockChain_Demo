fe58f622cddd837f541ff081179cec11dbbb90cf
Iqbal Badar <ibadar@ucsd.edu>
2021-06-27 13:34:05 -0700

    it looks like we've got the blockchain almost working

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,6 @@
 import hashlib
 import datetime
+import json
 
 class Block:
     def __init__(self, prev_hash, transactions , timestamp):
@@ -14,16 +15,18 @@ class Block:
     
     def make_hash(self):
         rawData = str(self.prev_hash) + str(self.timestamp) + str(self.transactions) + str(self._nonce)
-        return hashlib.sha256(rawData.encode()).hexdigest()
+        encoded = json.dumps(rawData, sort_keys=True).encode()
+        return hashlib.sha256(rawData).hexdigest()
 
-    def mine_block(proof_of_work_diff, reward):
-        hash_valid_temp = " " * proof_of_work_diff
+    def mine_block(self, proof_of_work_diff):
+        hash_valid_temp = "0" * proof_of_work_diff
+        print(len(hash_valid_temp))
         print("going through proof of work algo")
-        while( hash[:proof_of_work_diff] != hash_valid_temp ):
-            _nonce += 1
-            hash = self.make_hash
-        print("{0} this hash was mined" , hash )
-        return true
+        while( self.hash[:proof_of_work_diff] != hash_valid_temp ):
+            self._nonce += 1
+            self.hash = self.make_hash()
+        print("this hash was mined" , self.hash )
+        return True
 
 class Transaction :
     def __init__(self, from_person, to_person, amount):
@@ -38,34 +41,36 @@ class Transaction :
         return str({"From Person": self.from_person , "To Person": self.to_person, "Amount" : self.amount})
 
 class BlockChain:
-    def __init__(proof_of_work_diff, reward):
+    def __init__(self, proof_of_work_diff, reward):
         self.proof_of_work_diff = proof_of_work_diff
         self.reward = reward
-        self.pending_blocks = []
-        self.block_chain = []
+        self.pending_transactions = []
+        self.block_chain = [self.create_first_block()]
 
-    def add_block(block, ):
-        self.pending_blocks.append(block)
+    def add_transaction(self, transaction):
+        self.pending_transactions.append(transaction)
 
-    def create_first_block():
-        return Block("", [], datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+    def create_first_block(self):
+        first_transaction = Transaction("","",0)
+        return Block("", [first_transaction], datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
 
-    def validate_blocks():
-        while(len(self.pending_blocks) > 0):
-            block_to_validate = self.pending_blocks[0]
+    def mine(self, miner):
+        transaction_for_miner = Transaction("Iqbal_Coin", miner.name, self.reward)
+        self.pending_transactions.append(transaction_for_miner)
+        print(self.block_chain)
 
-            if(len(pending_blocks) > 1):
-                pending_blocks = pending_blocks[1:]
-            else:
-                pending_blocks.pop()
-            
-            if(block.mine_block(self.proof_of_work_diff, self.reward)):
-                print("block mined successfully")
-                self.block_chain.append(block)
+        prev_hash = self.block_chain[-1].hash
+        block_to_mine = Block(prev_hash, self.pending_transactions, datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+        print(block_to_mine)
 
+        if(block_to_mine.mine_block(self.proof_of_work_diff)):
+            print("Block is mined, moving to the chain!")
+            self.block_chain.append(block_to_mine)
+        else:
+            print("Block was not mined, not allowed on the chain")
 
 class Miner:
-    def __init__(name):
+    def __init__(self, name):
         self.name = name
         self.amount = 0
 
@@ -80,11 +85,14 @@ def main():
     transaction_4 = Transaction("iqbal", "shawty", 55.6)
 
     transaction_li = [transaction_2,transaction_3, transaction_4] 
-
-    b1 = Block("", transaction_1, datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
-    b2 = Block(b1.hash, transaction_li, datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
-
-    print(b2)
+    block_chain = BlockChain(5, 10)
+    block_chain.add_transaction(transaction_1)
+    block_chain.add_transaction(transaction_2)
+    block_chain.add_transaction(transaction_3)
+    block_chain.add_transaction(transaction_4)
+    
+    miner = Miner("Jeff Bezos")
+    block_chain.mine(miner)
 
 if __name__ == "__main__":
     main()
